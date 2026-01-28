876b436065122de677b6c80a1829f5164a73a3ec
Bagó Ádám Adrián <151737980+CsakEgyBago@users.noreply.github.com>
2026-01-28 13:54:56 +0100

    attempt 3 to fix login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import './index.css'
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { AuthContext } from './context/AuthContext';
 
