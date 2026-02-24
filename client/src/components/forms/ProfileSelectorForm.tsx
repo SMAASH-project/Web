59bd1d48831f31e134ea37839fa28b69da7f502a
Bagó Ádám Adrián <151737980+CsakEgyBago@users.noreply.github.com>
2026-02-24 14:03:50 +0100

    cleaned up profile selector

--- client/src/components/forms/ProfileSelectorForm.tsx
+++ client/src/components/forms/ProfileSelectorForm.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AddNewProfile } from "./addNewProfile/AddNewProfile";
 import { useProfiles } from "./addNewProfile/useProfiles";
+import * as motion from "motion/react-client";
 
 export function ProfileSelectorForm() {
   const { settings } = useSettings();
@@ -43,64 +44,85 @@ export function ProfileSelectorForm() {
         <div className="flex flex-row items-center justify-center gap-6 z-1 flex-wrap">
           <div className="flex flex-row items-center gap-6">
             {profiles.map((p) => (
-              <div
-                key={p.name}
-                className="flex flex-col items-center gap-2"
-                onClick={() => handleProfileClick(p.name)}
-              >
-                <Avatar
-                  size="lg"
-                  className={`text-white cursor-pointer ${settings.useLiquidGlass ? `bg-white/30 backdrop-blur-lg border-2 shadow-sm shadow-white/20[text-shadow:0_2px_4px_rgba(163,163,163,0.8)] ${isManaging ? "border-red-400" : "border-white/30"}` : `${isManaging ? "border-red-500" : "border-green-500"} border-2 bg-amber-200`} `}
+              <div key={p.name} className="flex flex-col items-center gap-2">
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
                 >
-                  <AvatarImage src={p.avatar} alt={p.name} />
-                  <span
-                    aria-hidden
-                    className={cn(
-                      "absolute inset-0 flex items-center justify-center rounded-full bg-gray-700/70 opacity-0 transition-opacity duration-150 pointer-events-none",
-                      "group-hover/avatar:opacity-100",
-                    )}
+                  <Avatar
+                    size="lg"
+                    onClick={() => handleProfileClick(p.name)}
+                    className={`text-white cursor-pointer ${settings.useLiquidGlass ? `bg-white/30 backdrop-blur-lg border-2 shadow-sm shadow-white/20[text-shadow:0_2px_4px_rgba(163,163,163,0.8)] ${isManaging ? "border-red-400" : "border-white/30"}` : `${isManaging ? "border-red-500" : "border-green-500"} border-2 bg-amber-200`} `}
                   >
-                    {isManaging ? (
-                      <Trash2
-                        className={cn("size-10 text-white opacity-100")}
-                      />
-                    ) : (
-                      <Play className={cn("size-10 text-white opacity-100")} />
-                    )}
+                    <AvatarImage src={p.avatar} alt={p.name} />
+                    <span
+                      aria-hidden
+                      className={cn(
+                        "absolute inset-0 flex items-center justify-center rounded-full bg-gray-700/70 opacity-0 transition-opacity duration-150 pointer-events-none",
+                        "group-hover/avatar:opacity-100",
+                      )}
+                    >
+                      {isManaging ? (
+                        <Trash2
+                          className={cn("size-10 text-white opacity-100")}
+                        />
+                      ) : (
+                        <Play
+                          className={cn("size-10 text-white opacity-100")}
+                        />
+                      )}
+                    </span>
+                    <AvatarFallback>
+                      {p.name
+                        ? p.name
+                            .split(" ")
+                            .map((n) => n[0])
+                            .slice(0, 2)
+                            .join("")
+                        : "NA"}
+                    </AvatarFallback>
+                  </Avatar>
+
+                  <span
+                    className={`text-white ${settings.useLiquidGlass ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]" : ""}`}
+                  >
+                    {p.name}
                   </span>
-                  <AvatarFallback>
-                    {p.name
-                      ? p.name
-                          .split(" ")
-                          .map((n) => n[0])
-                          .slice(0, 2)
-                          .join("")
-                      : "NA"}
-                  </AvatarFallback>
-                </Avatar>
-                <span className="text-white text-sm text-center">{p.name}</span>
+                </motion.button>
               </div>
             ))}
             {profileCount < 5 && (
-              <div className="flex flex-col items-center gap-2">
-                <button
-                  onClick={() => setShowAddProfile(true)}
-                  className="w-12 h-12 rounded-full bg-gray-600 border-2 border-gray-500 flex items-center justify-center cursor-pointer hover:bg-gray-700 transition-colors"
-                >
-                  <Plus className="size-6 text-white" />
-                </button>
-                <span className="text-white text-sm text-center">Add New</span>
-              </div>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <div className="flex flex-col items-center gap-2">
+                  <button
+                    onClick={() => setShowAddProfile(true)}
+                    className="w-12 h-12 rounded-full bg-gray-600 border-2 border-gray-500 flex items-center justify-center cursor-pointer hover:bg-gray-700 transition-colors"
+                  >
+                    <Plus className="size-6 text-white" />
+                  </button>
+
+                  <span
+                    className={`text-white text-sm ${settings.useLiquidGlass ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]" : ""}`}
+                  >
+                    Add New
+                  </span>
+                </div>
+              </motion.button>
             )}
           </div>
         </div>
         <div className="mb-4 z-1 ">
-          <Button
-            onClick={() => setIsManaging((prev) => !prev)}
-            className={`text-white ${settings.useLiquidGlass ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)] rounded-lg bg-white/30" : ""} cursor-pointer`}
-          >
-            {isManaging ? "Done" : "Manage Profiles"}
-          </Button>
+          <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
+            <Button
+              onClick={() => setIsManaging((prev) => !prev)}
+              className={`text-white ${settings.useLiquidGlass ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)] rounded-lg bg-white/30" : ""} cursor-pointer`}
+            >
+              {isManaging ? "Done" : "Manage Profiles"}
+            </Button>
+          </motion.button>
         </div>
       </div>
       <AddNewProfile open={showAddProfile} onOpenChange={setShowAddProfile} />
