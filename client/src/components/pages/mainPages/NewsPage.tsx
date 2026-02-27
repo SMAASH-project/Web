6c81b12c48707c6077a0020c767fc09dacd5742a
Joe Biden <bagoadamadrian@gmail.com>
2026-02-27 15:32:18 +0100

    did more cleaning

--- client/src/components/pages/mainPages/NewsPage.tsx
+++ client/src/components/pages/mainPages/NewsPage.tsx
@@ -3,14 +3,14 @@ import Navbar from "../../nav/Navbar";
 import { Card } from "@/components/ui/card";
 import { useSettings } from "../profileDependents/settings/settingsLogic/SettingsContext";
 import { Label } from "@/components/ui/label";
-import { AddNews } from "./NewsPageComponents/AddNews";
-import { RemoveButton } from "./NewsPageComponents/RemoveButton";
-import { EditButton } from "./NewsPageComponents/EditButton";
+import { AddNews } from "./newsPageComponents/AddNews";
+import { RemoveButton } from "./newsPageComponents/RemoveButton";
+import { EditButton } from "./newsPageComponents/EditButton";
 import { ButtonGroup } from "@/components/ui/button-group";
-import { Search } from "./NewsPageComponents/Search";
+import { Search } from "./newsPageComponents/Search";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { useNewsPosts } from "./NewsPageComponents/newsPageLogic/useNewsPosts";
+import { useNewsPosts } from "./newsPageComponents/newsPageLogic/useNewsPosts";
 
 export function NewsPage() {
   const { settings } = useSettings();
