4f3b81b42077e0ae731f9b9d43b181907663e0f1
BDonat05 <bujakidonat@gmail.com>
2026-03-02 11:09:59 +0100

    authorization now supports both cookies and request header as way to pass jwt

--- internal/middlewares/authz.go
+++ internal/middlewares/authz.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	dtos "smaash-web/internal/DTOs"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -11,11 +12,26 @@ import (
 )
 
 func Authorize(c *gin.Context) {
+	var rawToken string
+	path := c.Request.URL.Path
+
+	// Check if token is sent in cookie, and prioritize it's handling over header
 	rawToken, err := c.Cookie("Authorization")
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, dtos.NewErrResp("No authorization token provided", c.Request.URL.Path))
-		c.Abort()
-		return
+		// Check authorization header, if no authorization cookie found
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			// Neither cookie, nor auth header found
+			c.JSON(http.StatusUnauthorized, dtos.NewErrResp("No authorization token provided", path))
+			c.Abort()
+			return
+		}
+		rawToken = strings.TrimPrefix(authHeader, "Bearer ")
+		if authHeader == rawToken {
+			c.JSON(http.StatusUnauthorized, dtos.NewErrResp("Authorization header is in wrong format - Bearer prefix is required", path))
+			c.Abort()
+			return
+		}
 	}
 
 	token, err := jwt.Parse(rawToken, func(token *jwt.Token) (any, error) {
@@ -35,7 +51,7 @@ func Authorize(c *gin.Context) {
 			return
 		}
 
-		c.Set("id", uint(claims["sub"].(float64))) // ugghh go type system
+		c.Set("caller_id", uint(claims["sub"].(float64))) // ugghh go type system
 		c.Next()
 	} else {
 		c.JSON(http.StatusUnauthorized, dtos.NewErrResp("Invalid token claims", c.Request.URL.Path))
