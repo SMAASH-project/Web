0705c9fbd5c92d2b4d6ab7498e9dd8afb0527da9
Bagó Ádám Adrián <151737980+CsakEgyBago@users.noreply.github.com>
2026-03-05 09:30:45 +0100

    revert and fixes

--- client/src/components/pages/profileDependents/profile/UpdateSheet.tsx
+++ client/src/components/pages/profileDependents/profile/UpdateSheet.tsx
@@ -33,26 +33,71 @@ export function UpdateSheet() {
             Edit
           </Button>
         </SheetTrigger>
-        <SheetContent>
+        <SheetContent
+          className={`${
+            settings.useLiquidGlass
+              ? "bg-white/30 backdrop-blur-lg border-white/30 shadow-sm shadow-white/20"
+              : ""
+          }`}
+        >
           <SheetHeader>
-            <SheetTitle>Edit profile</SheetTitle>
-            <SheetDescription>
+            <SheetTitle
+              className={`text-white text-lg ${
+                settings.useLiquidGlass
+                  ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]"
+                  : ""
+              }`}
+            >
+              Edit profile
+            </SheetTitle>
+            <SheetDescription
+              className={`text-white text-sm ${
+                settings.useLiquidGlass
+                  ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]"
+                  : ""
+              }`}
+            >
               Make changes to your profile here. <br />
               Click save when you&apos;re done.
             </SheetDescription>
           </SheetHeader>
           <div className="grid flex-1 auto-rows-min gap-6 px-4">
             <div className="grid gap-3">
-              <Label htmlFor="sheet-username">Username</Label>
+              <Label
+                htmlFor="sheet-username"
+                className={`text-white text-md ${
+                  settings.useLiquidGlass
+                    ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]"
+                    : ""
+                }`}
+              >
+                Username
+              </Label>
               <Input id="sheet-name" defaultValue={Username} />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="sheet-email">Email Adress</Label>
+              <Label
+                htmlFor="sheet-email"
+                className={`text-white text-md ${
+                  settings.useLiquidGlass
+                    ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]"
+                    : ""
+                }`}
+              >
+                Email Address
+              </Label>
               <Input id="sheet-email" type="email" defaultValue={Email} />
             </div>
             <div className="grid gap-3">
               <div className="flex items-center space-x-2">
-                <Label htmlFor="sheet-username" className="cursor-pointer">
+                <Label
+                  htmlFor="sheet-username"
+                  className={`cursor-pointer text-white text-md ${
+                    settings.useLiquidGlass
+                      ? "[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]"
+                      : ""
+                  }`}
+                >
                   Password
                 </Label>
                 <FieldGroup>
@@ -81,6 +126,7 @@ export function UpdateSheet() {
               </div>
               <Input
                 id="sheet-password"
+                className={`text-white cursor-pointer ${settings.useLiquidGlass ? "bg-white/20 backdrop-blur-xl border border-white/30 shadow-lg shadow-white/10 ring-1 ring-white/20 [text-shadow:0_2px_4px_rgba(163,163,163,0.8)] placeholder:text-white/50" : ""}`}
                 // 2. Change type dynamically based on state
                 type={showPassword ? "text" : "password"}
                 defaultValue={Password}
@@ -88,11 +134,18 @@ export function UpdateSheet() {
             </div>
           </div>
           <SheetFooter>
-            <Button type="submit" className="text-white cursor-pointer">
+            <Button
+              type="submit"
+              className={`text-white cursor-pointer ${settings.useLiquidGlass ? "bg-white/30 backdrop-blur-lg border-white/30 shadow-sm shadow-white/20[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]" : ""}`}
+            >
               Save changes
             </Button>
             <SheetClose asChild>
-              <Button className="text-white cursor-pointer">Close</Button>
+              <Button
+                className={`text-white cursor-pointer ${settings.useLiquidGlass ? "bg-white/30 backdrop-blur-lg border-white/30 shadow-sm shadow-white/20[text-shadow:0_2px_4px_rgba(163,163,163,0.8)]" : ""}`}
+              >
+                Close
+              </Button>
             </SheetClose>
           </SheetFooter>
         </SheetContent>
